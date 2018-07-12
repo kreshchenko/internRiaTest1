40a98b57a78515172b626019a457cce8f6781355
kreshchenko <kreshchenkoi@gmail.com>
2018-07-12 12:14:28 +0300

    init commit

---
+++ static/index.js
@@ -0,0 +1,12 @@
+//Объект для одной записи с книгой
+let bookItem = {
+  id: null,
+  author: undefined,
+  name: undefined,
+  img: undefined
+};
+
+//Парсим файл *.json
+let a = JSON.parse("data.json");
+
+alert("lol");
